945a2a8a75509c10e8416d50c5ea121bb964616f
guiisilva1 <florencio.silva.guilherme@escola.pr.gov.br>
2025-06-05 13:30:59 -0300

    Add files via upload

---
+++ Guilherme F. - 2°B/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Terra',
+    'Por que a Terra é chamada de "planeta azul"?',
+    'Porque ela possui uma grande quantidade de água que cobre a maior parte de sua superfície, formando oceanos, mares, rios e lagos.'
+)
+
+criaCartao(
+    'Lua',
+    'O que causa as diferentes fases da Lua que vemos no céu?',
+    'São causadas pela mudança na quantidade de luz solar que atinge a Lua e que é refletida em nossa direção.'
+)
+
+criaCartao(
+    'Marte',
+    'Qual é a cor principal da superfície de Marte?',
+    'A cor principal da superfície de Marte é o vermelho, devido à presença de óxido de ferro (ferrugem) em suas rochas e solo.'
+)
+
+criaCartao(
+    'Júpiter',
+    'Júpiter é um planeta rochoso ou gasoso?',
+    'Júpiter é um planeta gasoso, não rochoso. Ele é composto principalmente de hidrogênio e hélio, sem uma superfície sólida como a Terra.'
+)
